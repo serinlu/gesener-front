30aaf371b6bde1ca67f790c2877ddc8e1bc1a9ea
ser1nlu <serinlu2201@gmail.com>
2024-11-29 17:28:26 -0500

    refactor checkout layout

--- src/layouts/CheckoutLayout.jsx
+++ src/layouts/CheckoutLayout.jsx
@@ -1,14 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CheckoutStages from '@/components/CheckoutStages'
 import logo from '@/uploads/logo.png'
 import { Link, NavLink, Outlet } from 'react-router-dom'
 import Footer from '@/components/Footer'
 import { AuthContext } from '@/context/AuthContext'
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/react'
-import { FaUser } from 'react-icons/fa'
+import { FaUser, FaUserCircle } from 'react-icons/fa'
 
 const CheckoutLayout = () => {
     const { auth, logout } = useContext(AuthContext)
+    const [showUserDropdown, setShowUserDropdown] = useState(false);
 
     const ItemsUser = [
         { key: "login", label: "Iniciar sesión", path: "/login" },
@@ -22,8 +23,14 @@ const CheckoutLayout = () => {
         { key: "logout", label: "Cerrar sesión", action: logout }
     ];
 
+    const itemsClient = [
+        { key: "profile", label: "Mi cuenta", path: "/profile" },
+        { key: "orders", label: "Mis pedidos", path: "/orders" },
+        { key: "logout", label: "Cerrar sesión", action: logout }
+    ];
+
     return (
-        <div className='w-full bg-white text-gray-500'>
+        <div className='flex flex-col min-h-screen bg-white text-gray-500'>
             <div className='w-[80%] mx-auto'>
                 <div className='flex justify-between p-4'>
                     <Link to='/'>
@@ -32,60 +39,109 @@ const CheckoutLayout = () => {
                     <div className='flex items-center text-3xl font-bold'>
                         Checkout
                     </div>
-                    <Dropdown>
-                        <DropdownTrigger>
-                            {auth ? (
-                                <Button
-                                    variant="bordered"
-                                    className='p-2 text-lg hover:text-indigo-500 duration-300 rounded-lg'
-                                >
-                                    {auth.name} {/* Muestra el nombre del usuario */}
-                                </Button>
-                            ) : (
-                                <Button
-                                    variant="bordered"
-                                    className='p-2 text-lg hover:text-indigo-500 duration-300 rounded-lg'
-                                >
-                                    <FaUser />
-                                </Button>
-                            )}
-                        </DropdownTrigger>
+                    <div className="relative z-50 my-auto">
+                        <button
+                            onClick={() => setShowUserDropdown(!showUserDropdown)}
+                            className="flex items-center text-lg p-2 rounded-md bg-white hover:bg-gray-100 transition-all"
+                        >
+                            <FaUserCircle className="text-2xl sm:mr-2" />
+                            <h1 className="hidden sm:block">{auth ? auth.name : 'Ingresar'}</h1>
+                        </button>
 
-                        <DropdownMenu aria-label="Opciones de usuario" className='bg-white shadow-lg rounded-lg p-2 text-base'>
-                            {auth ? (
-                                itemsAuthUser.map(item => (
-                                    <DropdownItem
-                                        key={item.key}
-                                        className="p-2"
-                                        onClick={item.action ? item.action : null} // Si el ítem es logout, llama a la acción
-                                    >
-                                        <NavLink
-                                            to={item.path}
-                                            className="hover:bg-gray-200 transition-all duration-300 ease-in-out p-2 rounded-lg"
-                                        >
-                                            {item.label}
-                                        </NavLink>
-                                    </DropdownItem>
-                                ))
-                            ) : (
-                                ItemsUser.map(item => (
-                                    <DropdownItem key={item.key} className="p-2">
-                                        <NavLink
-                                            to={item.path}
-                                            className="hover:bg-gray-200 transition-all duration-300 ease-in-out p-2 rounded-lg"
-                                        >
-                                            {item.label}
-                                        </NavLink>
-                                    </DropdownItem>
-                                ))
-                            )}
-                        </DropdownMenu>
-                    </Dropdown>
+                        {/* Dropdown */}
+                        <div
+                            className={`absolute -right-12 mt-2 w-48 bg-white rounded-md shadow-lg transition-[max-height,opacity] duration-500 ease-in-out ${showUserDropdown ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0'
+                                } overflow-hidden`}
+                        >
+                            <ul className="py-2">
+                                {auth ? (
+                                    auth.role === 'admin' ? (
+                                        itemsAuthUser.map((item) => (
+                                            <li key={item.key}>
+                                                {/* Verifica si el item tiene una acción, de ser así, ejecuta la acción */}
+                                                {item.action ? (
+                                                    <button
+                                                        onClick={() => {
+                                                            item.action(); // Ejecuta la acción (logout en este caso)
+                                                            setShowUserDropdown(false); // Cierra el dropdown
+                                                        }}
+                                                        className="block px-4 py-2 text-gray-700 hover:bg-indigo-100 hover:text-indigo-500 w-full text-left"
+                                                    >
+                                                        {item.label}
+                                                    </button>
+                                                ) : (
+                                                    <NavLink
+                                                        to={item.path}
+                                                        onClick={() => setShowUserDropdown(false)} // Cierra el dropdown al hacer clic
+                                                        className="block px-4 py-2 text-gray-700 hover:bg-indigo-100 hover:text-indigo-500"
+                                                    >
+                                                        {item.label}
+                                                    </NavLink>
+                                                )}
+                                            </li>
+                                        ))
+                                    ) : (
+                                        itemsClient.map((item) => (
+                                            <li key={item.key}>
+                                                {/* Verifica si el item tiene una acción, de ser así, ejecuta la acción */}
+                                                {item.action ? (
+                                                    <button
+                                                        onClick={() => {
+                                                            item.action(); // Ejecuta la acción (logout en este caso)
+                                                            setShowUserDropdown(false); // Cierra el dropdown
+                                                        }}
+                                                        className="block px-4 py-2 text-gray-700 hover:bg-indigo-100 hover:text-indigo-500 w-full text-left"
+                                                    >
+                                                        {item.label}
+                                                    </button>
+                                                ) : (
+                                                    <NavLink
+                                                        to={item.path}
+                                                        onClick={() => setShowUserDropdown(false)} // Cierra el dropdown al hacer clic
+                                                        className="block px-4 py-2 text-gray-700 hover:bg-indigo-100 hover:text-indigo-500"
+                                                    >
+                                                        {item.label}
+                                                    </NavLink>
+                                                )}
+                                            </li>
+                                        ))
+                                    )
+                                ) : (
+                                    ItemsUser.map((item) => (
+                                        <li key={item.key}>
+                                            {/* Verifica si el item tiene una acción, de ser así, ejecuta la acción */}
+                                            {item.action ? (
+                                                <button
+                                                    onClick={() => {
+                                                        item.action(); // Ejecuta la acción (logout en este caso)
+                                                        setShowUserDropdown(false); // Cierra el dropdown
+                                                    }}
+                                                    className="block px-4 py-2 text-gray-700 hover:bg-indigo-100 hover:text-indigo-500 w-full text-left"
+                                                >
+                                                    {item.label}
+                                                </button>
+                                            ) : (
+                                                <NavLink
+                                                    to={item.path}
+                                                    onClick={() => setShowUserDropdown(false)} // Cierra el dropdown al hacer clic
+                                                    className="block px-4 py-2 text-gray-700 hover:bg-indigo-100 hover:text-indigo-500"
+                                                >
+                                                    {item.label}
+                                                </NavLink>
+                                            )}
+                                        </li>
+                                    ))
+                                )}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <CheckoutStages />
                 <Outlet />
             </div>
-            <Footer />
+            <footer className="w-full text-center mt-auto py-4 bg-white">
+                <p className="text-sm text-gray-400">&copy; 2024 Gesener. Todos los derechos reservados.</p>
+            </footer>
         </div>
     )
 }
