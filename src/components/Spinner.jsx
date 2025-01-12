50380b88475bf5059c461771fb44249223c95e2d
Diego Cedron <70548640+DiegoCedron7@users.noreply.github.com>
2025-01-12 04:32:09 -0500

    feat: integrate notistack for enhanced notification handling and update Spinner component for dynamic color support

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Spinner() {
+export default function Spinner({ color }) {
   return (
     <div role="status">
       <svg
         aria-hidden="true"
-        class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-white"
+        class={`inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-${color}-500`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from '@/App.jsx'
+import { SnackbarProvider } from 'notistack'
+import { createRoot } from 'react-dom/client'
+
 import '@/index.css'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <SnackbarProvider />
+    <App />
+  </>
 )
